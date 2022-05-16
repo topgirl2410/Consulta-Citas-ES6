4f111287207973813c62c29111b299c9399f6387
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-16 23:40:31 +0200

    primeros pasos con el proyecto

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-// Campos del formulario 
 
 const mascotaInput = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
@@ -8,29 +7,9 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
-// UI
-
 const formulario = document.querySelector('#nueva-cita');
+
 const contenedorCitas = document.querySelector('#citas');
-
-// Clases
-
-class Citas {
-    constructor() {
-        this.citas = [];
-    }
-}
-
-class UI {
-
-}
-
-
-const ui = new UI();
-const administrarCitas = new Citas();
-
-
-// Registrar Eventos 
 
 eventListeners();
 function eventListeners() {
@@ -40,11 +19,7 @@ function eventListeners() {
     fechaInput.addEventListener('input', datosCita);
     horaInput.addEventListener('input', datosCita);
     sintomasInput.addEventListener('input', datosCita);
-
-    formulario.addEventListener('submit', nuevaCita);
 }
-
-// Objeto con la informacion de la cita
 
 const citaObj = {
     mascota: '',
@@ -55,27 +30,9 @@ const citaObj = {
     sintomas: ''
 }
 
-// Agrega datos al objeto de citas
-
 function datosCita(e) {
     citaObj[e.target.name] = e.target.value;
-}
 
-
-// Valida y agrega una nueva cita a la clase de citas
-
-function nuevaCita(e) {
-    e.preventDefault();
-
-    // Extraer la información del objeto de cita
-    const { mascota, propietario, telefono, fecha, hora, sintomas } = citaObj;
-
-    // Validar 
-    if (mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === '') {
-        console.log('Todos los campos son obligatorios');
-
-        return;
-    }
-
+    console.log(citaObj)
 
 }
