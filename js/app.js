4aef871cf906718323566a3ccfe745c194f69cf5
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-20 17:16:44 +0200

    editar una cita

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
 
+let editando;
+
 // Clases del proyecto (citas, UI)
 
 class Citas {
@@ -193,11 +195,30 @@ function nuevaCita(e) {
         return;
     }
 
-    // Generar un id único para cada cita 
-    citaObj.id = Date.now();
+    if (editando) {
+        ui.imprimirAlerta('La cita se modifico correctamente');
 
-    // Crear una nueva cita 
-    administrarCitas.agregarCita({ ...citaObj });
+        // Pasar el objeto de la cita actualizado
+
+
+        // Cambiar el texto del boton
+        formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
+       
+        // Quitar modo edicion
+        editando = false;
+
+    } else {
+        // Generar un id único para cada cita 
+        citaObj.id = Date.now();
+
+        // Crear una nueva cita 
+        administrarCitas.agregarCita({ ...citaObj });
+
+        // Imprimir mensaje de agregado correctamente
+        ui.imprimirAlerta('La cita se agrego correctamente');
+    }
+
+
 
 
     // Reiniciar el objeto para la validación
@@ -232,3 +253,30 @@ function elimimarCita(id) {
     ui.imprimirCitas(administrarCitas);
 }
 
+// Carga los datos y el modo edicion
+function cargarEdicion(cita) {
+    const { mascota, propietario, telefono, fecha, hora, sintomas, id } = cita;
+
+    // Llenar los inputs
+    mascotaInput.value = mascota;
+    propietarioInput.value = propietario;
+    telefonoInput.value = telefono;
+    fechaInput.value = fecha;
+    horaInput.value = hora;
+    sintomasInput.value = sintomas;
+
+    // Llenar el objeto
+    citaObj.mascota = mascota;
+    citaObj.propietario = propietario;
+    citaObj.telefono = telefono;
+    citaObj.fecha = fecha;
+    citaObj.hora = hora;
+    citaObj.sintomas = sintomas;
+    citaObj.id = id;
+
+    // Cambiar el texto del boton
+    formulario.querySelector('button[type="submit"]').textContent = 'Guardar Cambios';
+
+    editando = true;
+
+}
